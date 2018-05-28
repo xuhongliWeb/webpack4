49b6d4d184e14b31f2c0e20db42cdaaeb42d7720
unknown <hongli.xu@alphainsight.ai>
2018-05-28 16:00:54 +0800

    打包多个入口文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
 module.exports = {
-    entry: './src/index.js', // 入口文件
-    output: { 
-        filename:'bundle.js',
-        path:path.resolve('dist')
+    //真正实现多入口和多出口需要写成对象的方式
+    entry: {
+        index: './src/index.js',
+        login: './src/login.js'
+    }, // 入口文件
+    output: {
+        // [name]就可以将出口文件名和入口文件名一一对应
+        filename: '[name].js',
+        path: path.resolve('dist')
     }, // 出口文件
     module: {}, // 处   理对应的模块
     plugins: [], // 处理对应的插件
